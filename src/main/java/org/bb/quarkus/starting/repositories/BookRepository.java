79fb425916b8c5e0fec8d04c48932a21b434ba62
SonFeuerball <bobbaumann.home@gmail.com>
2023-09-27 08:43:38 +0200

    profiling

--- src/main/java/org/bb/quarkus/starting/repositories/BookRepository.java
+++ src/main/java/org/bb/quarkus/starting/repositories/BookRepository.java
@@ -4,18 +4,22 @@ import java.util.List;
 import java.util.Optional;
 
 import org.bb.quarkus.starting.classes.Book;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class BookRepository {
 
+  @ConfigProperty(name = "books.genre", defaultValue = "Drama")
+  String genre;
+
   public List<Book> getAllBooks() {
     return List.of(
-        new Book(1, "EZ JavaScript", "Mr.Bookworm", 2007, "Horror"),
-        new Book(1, "EZ JavaScript", "Mr.Bookworm", 2007, "Horror"),
-        new Book(1, "EZ JavaScript", "Mr.Bookworm", 2007, "Horror"),
-        new Book(1, "EZ JavaScript", "Mr.Bookworm", 2007, "Horror"));
+        new Book(1, "EZ JavaScript", "Mr.Bookworm", 2007, genre),
+        new Book(1, "EZ JavaScript", "Mr.Bookworm", 2007, genre),
+        new Book(1, "EZ JavaScript", "Mr.Bookworm", 2007, genre),
+        new Book(1, "EZ JavaScript", "Mr.Bookworm", 2007, genre));
   }
 
   public Optional<Book> getBook(int id) {
